09732ad0220d9e5fc40a391684d6213f136e1e9f
Aiz <dev@aiz.moe>
2025-07-14 23:39:36 +0200

    fix(home): respect xdg config home

--- modules/home/aiz/default.nix
+++ modules/home/aiz/default.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   imports = [
     ./desktop
     ./programs
@@ -28,5 +32,25 @@
       gcloud.disabled = true;
       line_break.disabled = true;
     };
+  };
+  programs.fish.enable = true;
+
+  # Fixes home manager not respecting me
+  # setting XDG environment variables
+  xdg = let
+    local = "/home/aiz/local";
+  in {
+    enable = true;
+    configHome = local + "/config";
+    cacheHome = local + "/cache";
+    stateHome = local + "/state";
+    dataHome = local + "/share";
+  };
+
+  # Fix various applications to respect the XDG basedir spec
+  home.sessionVariables = {
+    HISTFILE = config.xdg.stateHome + "/bash/history";
+    GNUPGHOME = config.xdg.dataHome + "/gnupg";
+    STARSHIP_CACHE = config.xdg.cacheHome + "/starship";
   };
 }
