77ec239a4f2fbb712b3e57ffa79a4c225b17d7a1
Aiz <dev@aiz.moe>
2025-07-15 03:24:40 +0200

    feat(niri): input config

--- modules/home/aiz/desktop/niri/default.nix
+++ modules/home/aiz/desktop/niri/default.nix
@@ -1,6 +1,20 @@
 {config, ...}: {
   programs.niri.settings = {
     environment."NIXOS_OZONE_WL" = "1";
+
+    input = {
+      keyboard = {
+        repeat-delay = 275;
+        repeat-rate = 40;
+      };
+      touchpad = {
+        tap = true;
+        dwt = true; # disable while typing
+        natural-scroll = true;
+        click-method = "clickfinger";
+      };
+    };
+
     binds = with config.lib.niri.actions; {
       "MOD+Return".action = spawn "ghostty";
       "MOD+Space".action = spawn "fuzzel";
