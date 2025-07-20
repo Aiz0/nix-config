dd877cc69dccaf64f81f7b5434cb3f130f56491a
Aiz <dev@aiz.moe>
2025-07-20 19:32:08 +0200

    feat: home row modifiers with keyd

--- modules/nixos/os/services/default.nix
+++ modules/nixos/os/services/default.nix
@@ -1,5 +1,6 @@
 {...}: {
   imports = [
+    ./keyd
     ./regreet
   ];
 }
---
+++ modules/nixos/os/services/keyd/default.nix
@@ -0,0 +1,28 @@
+{...}: {
+  services.keyd = {
+    enable = true;
+    keyboards = {
+      default = {
+        ids = ["*"];
+        settings = {
+          main = let
+            delay = "200";
+          in {
+            #Home row modifiers
+            a = "overloadt2(alt, a, ${delay})";
+            s = "overloadt2(control, s, ${delay})";
+            d = "overloadt2(shift, d, ${delay})";
+            f = "overloadt2(meta, f, ${delay})";
+
+            j = "overloadt2(meta, j, ${delay})";
+            k = "overloadt2(shift, k, ${delay})";
+            l = "overloadt2(control, l, ${delay})";
+            semicolon = "overloadt2(alt, semicolon, ${delay})";
+
+            capslock = "overload(control, esc)";
+          };
+        };
+      };
+    };
+  };
+}
