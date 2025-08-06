ccbc6a0c442f36d308b9b6b46c86f79612453316
Aiz <dev@aiz.moe>
2025-08-06 10:31:55 +0200

    chore: mv gtk theming into aiz home

--- homes/aiz/default.nix
+++ homes/aiz/default.nix
@@ -62,5 +62,20 @@
         };
       };
     };
+
+    # Theming
+    dconf.settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+
+    gtk = {
+      enable = true;
+      theme = {
+        name = "Adwaita-dark";
+        package = pkgs.gnome-themes-extra;
+      };
+    };
   };
 }
--- modules/home/aiz/default.nix
+++ modules/home/aiz/default.nix
@@ -1,27 +1,8 @@
-{pkgs, ...}: {
+{...}: {
   imports = [
     ./desktop
     ./programs
     ./services
     ./assets
   ];
-  dconf.settings = {
-    "org/gnome/desktop/interface" = {
-      color-scheme = "prefer-dark";
-    };
-  };
-
-  gtk = {
-    enable = true;
-    theme = {
-      name = "Adwaita-dark";
-      package = pkgs.gnome-themes-extra;
-    };
-  };
-
-  # qt = {
-  #   enable = true;
-  #   platformTheme.name = "qt5ct";
-  #   style.name = "breeze";
-  # };
 }
