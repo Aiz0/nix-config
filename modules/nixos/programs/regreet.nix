34260146b90d6b67f0549aaa45f5c9639c09dfcb
Aiz <dev@aiz.moe>
2025-08-16 02:29:13 +0200

    feat(modules/nixos/programs/regreet): switch to niri from default cage

--- modules/nixos/programs/regreet.nix
+++ modules/nixos/programs/regreet.nix
@@ -1,10 +1,11 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   options.myNixOS.programs.regreet.enable = lib.mkOption {
-    description = "Enable regreet Display Manager";
+    description = "Enable regreet Display Manager, uses niri-unstable";
     default = false;
     type = lib.types.bool;
   };
@@ -15,9 +16,35 @@
       theme.name = "Adwaita-dark";
       settings = {
         background = {
-          #TODO: move to separate configuration
+          # TODO: move to separate configuration
           path = builtins.path {path = ../../../assets/frieren.jpg;};
           fit = "Cover";
+        };
+      };
+    };
+
+    # TODO: move to services directory
+    # TODO: setup output configuration and colors
+    services.greetd = let
+      niri-config = pkgs.writeText "niri-config" ''
+        hotkey-overlay {
+            skip-at-startup
+        }
+        environment {
+            GTK_USE_PORTAL "0"
+            GDK_DEBUG "no-portals"
+        }
+
+        // other settings
+
+        spawn-at-startup "sh" "-c" "${lib.getExe pkgs.regreet}; pkill -f niri"
+      '';
+    in {
+      enable = true;
+      settings = {
+        default_session = {
+          command = "${lib.getExe pkgs.niri-unstable} -c ${niri-config}";
+          user = "greeter";
         };
       };
     };
