8f8029dff98f7a15d99fc87c116ecd9854ec91f5
Aiz <dev@aiz.moe>
2025-08-17 03:19:20 +0200

    feat(modules/home/aiz/desktop/niri): configure default and preset column widths

--- modules/home/aiz/desktop/niri.nix
+++ modules/home/aiz/desktop/niri.nix
@@ -117,6 +117,13 @@
           top = 4;
           bottom = 4;
         };
+
+        default-column-width = {proportion = 1. / 2.;};
+        preset-column-widths = [
+          {proportion = 1. / 3.;}
+          {proportion = 1. / 2.;}
+          {proportion = 2. / 3.;}
+        ];
       };
       hotkey-overlay.skip-at-startup = true;
       clipboard.disable-primary = true;
