7e6fe098a35d7eef5434d0a0085429c862d9d54a
Aiz <dev@aiz.moe>
2025-08-20 06:21:11 +0200

    feat(modules/home/aiz/programs/hyprlock): only show labels and input on primary monitor

--- modules/home/aiz/programs/hyprlock/default.nix
+++ modules/home/aiz/programs/hyprlock/default.nix
@@ -23,7 +23,9 @@
   config = lib.mkIf config.myHome.aiz.programs.hyprlock.enable {
     programs.hyprlock = {
       enable = true;
-      settings = {
+      settings = let
+        primaryMonitors = lib.filter (m: m.primary) config.myHome.hardware.monitors;
+      in {
         background =
           builtins.map (monitor: {
             monitor = monitor.plug;
@@ -40,82 +42,86 @@
           })
           config.myHome.hardware.monitors;
         # User Avatar
-        image = lib.mkIf (config.myHome.aiz.programs.hyprlock.avatar.path != null) {
-          monitor = "";
-          inherit (config.myHome.aiz.programs.hyprlock.avatar) path;
-          border_color = "0xffdddddd";
-          border_size = 2;
-          size = 100;
-          rounding = -1;
-          position = "0, 160";
-          halign = "center";
-          valign = "bottom";
-        };
-        label = [
-          # Current date
-          {
-            monitor = "";
-            text = "cmd[update:1000] echo \"$(date +\"%A, %B %d\")\"";
-            color = "rgba(242, 243, 244, 0.75)";
-            font_size = 20;
-            font_family = "SF Pro Display Bold";
-            position = "0, -100";
-            halign = "center";
-            valign = "top";
-          }
-
-          # Current time
-          {
-            monitor = "";
-            text = "$TIME";
-            color = "rgba(242, 243, 244, 0.75)";
-            font_size = 93;
-            font_family = "SF Pro Display Bold";
-            position = "0, -150";
-            halign = "center";
-            valign = "top";
-          }
-          # Show username
-          {
-            monitor = "";
-            text = config.myHome.aiz.programs.hyprlock.displayName;
-            color = "rgba(242, 243, 244, 0.75)";
-            font_size = 12;
-            font_family = "SF Pro Display Bold";
-            position = "0, 125";
+        image = lib.mkIf (config.myHome.aiz.programs.hyprlock.avatar.path != null) (builtins.map (monitor: {
+            monitor = monitor.plug;
+            inherit (config.myHome.aiz.programs.hyprlock.avatar) path;
+            border_color = "0xffdddddd";
+            border_size = 2;
+            size = 100;
+            rounding = -1;
+            position = "0, 160";
             halign = "center";
             valign = "bottom";
-          }
+          })
+          primaryMonitors);
+        label = builtins.concatLists (builtins.map (monitor: [
+            # Current date
+            {
+              monitor = monitor.plug;
+              text = "cmd[update:1000] echo \"$(date +\"%A, %B %d\")\"";
+              color = "rgba(242, 243, 244, 0.75)";
+              font_size = 20;
+              font_family = "SF Pro Display Bold";
+              position = "0, -100";
+              halign = "center";
+              valign = "top";
+            }
 
-          # Password label
-          {
-            monitor = "";
-            text = "Enter Password";
-            color = "rgba(242, 243, 244, 0.75)";
-            font_size = 10;
-            font_family = "SF Pro Display";
-            position = "0, 100";
+            # Current time
+            {
+              monitor = monitor.plug;
+              text = "$TIME";
+              color = "rgba(242, 243, 244, 0.75)";
+              font_size = 93;
+              font_family = "SF Pro Display Bold";
+              position = "0, -150";
+              halign = "center";
+              valign = "top";
+            }
+            # Show username
+            {
+              monitor = monitor.plug;
+              text = config.myHome.aiz.programs.hyprlock.displayName;
+              color = "rgba(242, 243, 244, 0.75)";
+              font_size = 12;
+              font_family = "SF Pro Display Bold";
+              position = "0, 125";
+              halign = "center";
+              valign = "bottom";
+            }
+
+            # Password label
+            {
+              monitor = monitor.plug;
+              text = "Enter Password";
+              color = "rgba(242, 243, 244, 0.75)";
+              font_size = 10;
+              font_family = "SF Pro Display";
+              position = "0, 100";
+              halign = "center";
+              valign = "bottom";
+            }
+          ])
+          primaryMonitors);
+
+        input-field =
+          builtins.map (monitor: {
+            monitor = monitor.plug;
+            size = "300, 30";
+            outline_thickness = 0;
+            dots_size = 0.25; # Scale of input-field height, 0.2 - 0.8
+            dots_spacing = 0.55; # Scale of dots' absolute size, 0.0 - 1.0
+            dots_center = true;
+            outer_color = "rgba(242, 243, 244, 0)";
+            inner_color = "rgba(242, 243, 244, 0)";
+            font_color = "rgba(242, 243, 244, 0.75)";
+            fade_on_empty = false;
+            placeholder_text = "";
+            position = "0, 60";
             halign = "center";
             valign = "bottom";
-          }
-        ];
-
-        input-field = {
-          monitor = "";
-          size = "300, 30";
-          outline_thickness = 0;
-          dots_size = 0.25; # Scale of input-field height, 0.2 - 0.8
-          dots_spacing = 0.55; # Scale of dots' absolute size, 0.0 - 1.0
-          dots_center = true;
-          outer_color = "rgba(242, 243, 244, 0)";
-          inner_color = "rgba(242, 243, 244, 0)";
-          font_color = "rgba(242, 243, 244, 0.75)";
-          fade_on_empty = false;
-          placeholder_text = "";
-          position = "0, 60";
-          halign = "center";
-          valign = "bottom";
-        };
+          })
+          primaryMonitors;
       };
     };
   };
