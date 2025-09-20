2add1ecebae17c316cf713cf35ce110a9c05724a
Aiz <dev@aiz.moe>
2025-09-20 19:02:16 +0200

    feat(hosts/frieren/home): add monitor name and fix height

--- hosts/frieren/home.nix
+++ hosts/frieren/home.nix
@@ -4,14 +4,14 @@
     config.myHome.hardware.monitors = [
       {
         name = {
-          manufacturer = "Unknown";
-          model = "Unknown";
+          manufacturer = "BOE";
+          model = "0x0BCA";
           serial = "Unknown";
         };
         plug = "eDP-1";
         primary = true;
         width = 2256;
-        height = 1506;
+        height = 1504;
         wallpaper.path = builtins.path {path = ./assets/wallpaper.jxl;};
       }
     ];
