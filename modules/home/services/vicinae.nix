9c44a85b9c46c19e704141119bb9912b71ca2c3a
Aiz <dev@aiz.moe>
2025-12-25 16:02:47 +0100

    fix(modules/home/services/vicinae): enable autostart

--- modules/home/services/vicinae.nix
+++ modules/home/services/vicinae.nix
@@ -10,6 +10,15 @@
   options.myHome.services.vicinae.enable = lib.mkEnableOption "Vicinae raycast inspired launcher";
 
   config = lib.mkIf config.myHome.services.vicinae.enable {
-    services.vicinae.enable = true;
+    services.vicinae = {
+      enable = true;
+      systemd = {
+        enable = true;
+        autoStart = true; # default: false
+        environment = {
+          USE_LAYER_SHELL = 1;
+        };
+      };
+    };
   };
 }
