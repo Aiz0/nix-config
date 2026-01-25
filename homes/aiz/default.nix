89e26434de2175a10b73ae56f70d4f8b87217ff2
Aiz <dev@aiz.moe>
2026-01-25 23:57:41 +0100

    fix(homes/aiz): remove qt theming to in an attempt to fix dolphin icons

--- homes/aiz/default.nix
+++ homes/aiz/default.nix
@@ -161,10 +161,5 @@
       };
       gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
     };
-    qt = {
-      enable = true;
-      platformTheme.name = "Adwaita";
-      style.name = "adwaita-dark";
-    };
   };
 }
