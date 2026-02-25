01d03083bf58bce1121049db61cb47e359fef4e0
Aiz <dev@aiz.moe>
2026-02-25 02:15:55 +0100

    fix(hosts/miyabi/secrets/): wrong path for secrets, fix grafana key

--- hosts/miyabi/secrets.nix
+++ hosts/miyabi/secrets.nix
@@ -2,12 +2,15 @@
   age.secrets = {
     tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/auth.age";
     tailscaleCaddyAuthKey.file = "${self.inputs.secrets}/tailscale/caddyAuth.age";
-    bazarrApiKey.file = "${self.inputs.secrets}/bazarr/tokenKey.age";
-    lidarrApiKey.file = "${self.inputs.secrets}/lidarr/tokenKey.age";
-    prowlarrApiKey.file = "${self.inputs.secrets}/prowlarr/tokenKey.age";
+    bazarrApiKey.file = "${self.inputs.secrets}/arr/bazarrApiKey.age";
+    lidarrApiKey.file = "${self.inputs.secrets}/arr/lidarrApiKey.age";
+    prowlarrApiKey.file = "${self.inputs.secrets}/arr/prowlarrApiKey.age";
     radarrApiKey.file = "${self.inputs.secrets}/arr/radarrApiKey.age";
     sonarrApiKey.file = "${self.inputs.secrets}/arr/sonarrApiKey.age";
     kavitaTokenKey.file = "${self.inputs.secrets}/kavita/tokenKey.age";
-    grafanaSecretKey.file = "${self.inputs.secrets}/grafana/secretKey.age";
+    grafanaSecretKey = {
+      file = "${self.inputs.secrets}/grafana/secretKey.age";
+      owner = "grafana";
+    };
   };
 }
