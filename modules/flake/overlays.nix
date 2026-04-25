5edeaa34f53db1aff585cab6edc345f5b5bba565
Aiz <dev@aiz.moe>
2026-04-25 23:35:26 +0200

    fix(overlays): skip build checks for openldap

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -2,6 +2,13 @@
   flake.overlays = {
     default = _final: prev: {
       inherit (self.inputs.oculante-fix.legacyPackages.${prev.system}) oculante;
+
+      # Lutris and Bottles fails to build right now
+      # skips checks for oopenldap, remove when this is fixed
+      # https://github.com/NixOS/nixpkgs/issues/513245
+      openldap = prev.openldap.overrideAttrs {
+        doCheck = !prev.stdenv.hostPlatform.isi686;
+      };
     };
   };
 }
