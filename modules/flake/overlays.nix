40328e7d4df1c73997c8376ef8a82c4fa46e63e3
Aiz <dev@aiz.moe>
2026-04-25 23:35:26 +0200

    chore(overlays): remove oculante overlay
    it's fixed now

--- modules/flake/overlays.nix
+++ modules/flake/overlays.nix
@@ -1,8 +1,6 @@
-{self, ...}: {
+{...}: {
   flake.overlays = {
     default = _final: prev: {
-      inherit (self.inputs.oculante-fix.legacyPackages.${prev.system}) oculante;
-
       # Lutris and Bottles fails to build right now
       # skips checks for oopenldap, remove when this is fixed
       # https://github.com/NixOS/nixpkgs/issues/513245
